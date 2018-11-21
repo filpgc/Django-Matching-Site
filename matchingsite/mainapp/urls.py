e539507211bc5e54fb5eb6f53b8a8ab33fae1688
Filippo Piggici <filippo.piggicI@gmail.com>
2018-11-21 17:38:00 +0000

    Removed comments

--- matchingsite/mainapp/urls.py
+++ matchingsite/mainapp/urls.py
@@ -8,11 +8,3 @@ urlpatterns = [
 
 
 
-
-#    # ex: /polls/5/
-# the 'name' value as called by the {% url %} template tag
-# path('<int:question_id>/', views.detail, name='detail'),
-# ex: /polls/5/results/
-# path('<int:question_id>/results/', views.results, name='results'),
-# ex: /polls/5/vote/
-# path('<int:question_id>/vote/', views.vote, name='vote'),
