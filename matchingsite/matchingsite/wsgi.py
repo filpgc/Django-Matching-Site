51b8ecf115e55fbb2a16b3b4d155e6653ccdd019
Bassem Boulfarkat <bisbis@Bassems-MBP.connect>
2018-12-15 21:08:04 +0000

    mi

--- matchingsite/matchingsite/wsgi.py
+++ matchingsite/matchingsite/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-path = '/Users/bisbis/Desktop/Matching-Site/Matching-Site/matchingsite'
+path = 'matchingsite/matchingsite'
 if path not in sys.path:
 sys.path.append(path)
 
