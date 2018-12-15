c495a7d4f8736e391f4fbe50c9cc12c1d2cf6c92
Bassem Boulfarkat <bisbis@Bassems-MBP.connect>
2018-12-15 21:43:24 +0000

    la

--- matchingsite/matchingsite/wsgi.py
+++ matchingsite/matchingsite/wsgi.py
@@ -12,7 +12,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matchingsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainapp.settings')
 WSGI_APPLICATION = 'mainapp.wsgi.application'
 
 application = get_wsgi_application()
